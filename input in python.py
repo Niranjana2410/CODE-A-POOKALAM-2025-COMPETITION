889b0d9b92a4b81ed6bb769251a5a430c25b385c
Niranjana2410 <niranju909@gmail.com>
2025-09-06 11:08:57 +0530

    Add files via upload

---
+++ input in python.py
@@ -0,0 +1,7 @@
+#string input
+name = input("Name :")
+#int input
+age =int(input("Age :"))
+#float input
+num = float(input("Favorite number :"))
+print("My name is ",name, "and i am ",age, "year's old. My favorite decimal number is" ,num)
